41deb33289b1697a948ecc2b5b951783e59d3a08
BALA MURALI KRISHNA BIYYAPU <bbmkr@KRISHNA>
2022-11-30 17:26:49 +0530

    Restaurants page creation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,34 @@
+import Restaurant from "./Restaurant";
+import React from "react";
 function App() {
-  return <h1>Swiggy</h1>;
+  const [data, setData] = React.useState([]);
+  React.useEffect(() => {
+    fetch("http://localhost:3080/restaurants")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+      });
+  }, [data]);
+
+  return (
+    <div className="container">
+      <h1
+        style={{
+          marginBottom: "20px",
+          padding: "16px",
+          borderBottom: "1px solid lightgrey",
+        }}
+      >
+        {data.length} restaurants
+      </h1>
+
+      <div className="row">
+        {data.map((item) => (
+          <Restaurant key={item.id} info={item} />
+        ))}
+      </div>
+    </div>
+  );
 }
+
 export default App;
