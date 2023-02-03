0aae61b9dc64f1b8daf7b71f168ddd3dd3db3c26
BALA MURALI KRISHNA BIYYAPU <bbmkr@KRISHNA>
2023-02-03 15:51:52 +0530

    Redux persist implementation

--- src/Store.js
+++ src/Store.js
@@ -1,8 +1,35 @@
 import { configureStore } from "@reduxjs/toolkit";
 import appReducer from "./features/home/appSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-export const store = configureStore({
-  reducer: {
-    app: appReducer,
-  },
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, appReducer);
+
+const store = configureStore({
+  reducer: { app: persistedReducer },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+const persistor = persistStore(store);
+export default store;
+export { persistor };
