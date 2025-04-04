b015f7aeddd5dd00528928afa5b7b8337b52bcc1
krishnaBiyyapu <krishna@konic.net>
2025-04-04 15:59:57 +0530

    Modify(Dishes): redesign

--- src/Dishes.js
+++ src/Dishes.js
@@ -5,10 +5,12 @@ import Box from "@mui/material/Box";
 import Dish from "./Dish";
 import { useSelector, useDispatch } from "react-redux";
 import { getDishes } from "./features/home/appSlice";
+
 const Dishes = () => {
   const dispatch = useDispatch();
-  const dishes  = useSelector((store) => store.app.dishes);
+  const dishes = useSelector((store) => store.app.dishes);
   const { restaurantId } = useParams();
+
   useEffect(() => {
     dispatch(getDishes(restaurantId));
   }, [dispatch, restaurantId]);
@@ -21,16 +23,27 @@ const Dishes = () => {
             marginBottom: "24px",
             padding: "16px",
             borderBottom: "1px solid lightgrey",
+            color: "orange",
+            textAlign: "left",
           }}
         >
-          {dishes.length} dishes
+          {dishes.length} Dishes
         </h1>
 
-        <Box sx={{ flexGrow: 1 }}>
+        <Box
+          sx={{
+            flexGrow: 1,
+            background: "linear-gradient(135deg, #f5f5f5 0%, #e0e0e0 100%)",
+            padding: "20px",
+            borderRadius: "8px",
+          }}
+        >
           <Grid container spacing={3}>
-            {dishes.map((dish) => {
-              return <Dish key={dish.id} dish={dish} />;
-            })}
+            {dishes.map((dish) => (
+              <Grid item xs={12} sm={6} md={4} lg={3} key={dish.id}>
+                <Dish dish={dish} />
+              </Grid>
+            ))}
           </Grid>
         </Box>
       </div>
